8bedd9e4c505aa9345c44cbb4ad114a45762748a
Carlos Pérez-Aradros Herce <cperez@zentyal.com>
2013-09-28 20:51:45 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+from setuptools import setup, find_packages
+
+
+version = __import__('achilles').get_version()
+
+setup(
+    name='django-achilles',
+    version=version,
+    url='http://github.com/exekias/django-achilles/',
+    author='Carlos Pérez-Aradros Herce',
+    author_email='cperez@zentyal.com',
+    description='Django AJAX Framework',
+    packages=find_packages(),
+)
