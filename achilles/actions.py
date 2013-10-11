78760d798123b357c4f044bf032e1e0a8b448c3b
Carlos Pérez-Aradros Herce <cperez@zentyal.com>
2013-10-11 17:26:56 +0200

    Added first actions implementation

---
+++ achilles/actions.py
@@ -0,0 +1,54 @@
+from django.conf import settings
+from django.utils.log import getLogger
+from importlib import import_module
+
+from .common import BaseLibrary
+
+logger = getLogger(__name__)
+
+
+class Library(BaseLibrary):
+    """
+    Actions register library
+    """
+    def __init__(self, namespace=None):
+        BaseLibrary.__init__(self, Action, namespace)
+
+        # Provide action register
+        self.action = self.register
+
+    def create_class(self, func):
+        class A(Action):
+            def run(self):
+                return func(self.request, *self.args, **self.kwargs)
+        return A
+
+
+def get(name):
+    """
+    Return action function for the given name
+    """
+    # make sure all actions are loaded
+    for app in settings.INSTALLED_APPS:
+        try:
+            import_module(app + '.actions')
+        except:
+            pass
+
+    return Library.get_global(name)
+
+
+class Action(object):
+    """
+    Block section, defines a block in the page. template_name instance field
+    should be defined, so render method will use it in conjunction with
+    get_context_data
+    """
+
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        raise NotImplemented('This method should process the action')
---
+++ achilles/tests/actions.py
@@ -0,0 +1,37 @@
+import os
+
+from django.test import RequestFactory
+from django.test import TestCase
+
+from achilles import actions
+
+
+class ActionsTests(TestCase):
+
+    @classmethod
+    def setupClass(cls):
+        cls.register = actions.Library()
+        cls.request_factory = RequestFactory()
+
+    def setUp(self):
+        self.request = self.request_factory.get('/path')
+
+    def test_run_function_action(self):
+        @self.register.action
+        def action(request):
+            return 10
+
+        a = self.register.get('action')
+        self.assertEqual(a, action)
+        self.assertEqual(10, a(self.request).run())
+
+    def test_run_class_action(self):
+        @self.register.action('classaction')
+        class AnAction(actions.Action):
+            def run(self):
+                return 10
+
+        a = self.register.get('classaction')
+        self.assertEqual(a, AnAction)
+        self.assertEqual(10, a(self.request).run())
+
