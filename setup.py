aebac9cbee269d66c6ec39d514d793bce7940f80
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2013-11-07 01:53:10 +0100

    Fixed packaging

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setup(
     author_email='exekias@gmail.com',
     description='Django AJAX Framework',
     long_description=dump('README').read(),
-    packages=find_packages(exclude=['example', '*.tests']),
+    packages=find_packages(exclude=['example', '*.tests', '*.tests.*']),
     include_package_data=True,
     install_requires=[r.strip() for r in dump('requirements.txt')],
-    setup_requires=[r.strip() for r in dump('requirements-dev.txt')],
+    tests_require=[r.strip() for r in dump('requirements-dev.txt')],
     zip_safe=False,
     license='Apache License (2.0)',
     classifiers=[
