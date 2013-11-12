e5976d2867498ae63a2ddbdacb3faa529885cae2
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2013-11-12 01:33:05 +0100

    Updated example project with error management

--- example/achillesexample/actions.py
+++ example/achillesexample/actions.py
@@ -4,4 +4,8 @@ register = actions.Library('example')
 
 @register.action
 def multiply(request, a, b):
-    return int(a) * int(b)
+    return float(a) * float(b)
+
+@register.action
+def divide(request, a, b):
+    return float(a) / float(b)
