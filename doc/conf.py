550f0df566b3cefeed245108355a95080bdd6c7e
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-01-01 22:34:19 +0100

    Call django.conf.settings.configure when generating autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,9 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+from django.conf import settings
+settings.configure()
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
