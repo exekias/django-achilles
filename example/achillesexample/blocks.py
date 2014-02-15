8f27c2ded443d90161f0fd57031fc02e93d3f682
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-02-15 03:51:33 +0100

    Update example page with latest features

--- example/achillesexample/blocks.py
+++ example/achillesexample/blocks.py
@@ -1,13 +1,22 @@
 from achilles import blocks
+from time import sleep
+
 
 register = blocks.Library('example')
 
 COUNTER = 0
 
-@register.block(template_name='blocks/counter.html')
+@register.block(template_name='blocks/message.html')
 def counter():
     global COUNTER
     COUNTER += 1
     return {
-        'counter' : COUNTER,
+        'message': 'Block loaded %s times' % COUNTER,
+    }
+
+@register.block(template_name='blocks/message.html')
+def slow():
+    sleep(1)
+    return {
+        'message':'This block was loaded after page was loaded!',
     }
